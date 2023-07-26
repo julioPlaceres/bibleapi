769c89e6803f48c0c765b6797f810dd2c59f592d
julio <jplaceres@guidelinecentral.com>
2023-07-26 15:22:36 -0400

    removed boiler plate code from clent side, added initial frontend routes, and initia field types

---
+++ client/app/form/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Form() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Form Page</h1>
+    </div>
+
+  )
+}
---
+++ client/app/view-data/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ViewData() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>View Data Page</h1>
+    </div>
+  )
+}
