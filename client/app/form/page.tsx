c03c4ad12f047adadc64a40263922b7a9a2269f6
julio <jplaceres@guidelinecentral.com>
2023-07-27 16:03:11 -0400

    Added Character Logic and basic homepage design for UI

--- client/app/form/page.tsx
+++ client/app/form/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BookForm from '../components/forms/Book';
+import CharacterForm from '../components/forms/Character';
 
 const Form: React.FC = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Book Form</h2>
       <BookForm />
+      <CharacterForm />
       {/* Add more forms here as you create them */}
     </div>
   );
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,20 @@
 import RootLayout from './layout'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <RootLayout>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to the Bible API</h1>
-        <p>Select a page to continue</p>
+    <main className="min-h-screen flex flex-col items-center justify-center p-24 bg-cover bg-center" style={{backgroundImage: 'url(/cross.jpg)' }}>
+      <h1 className='text-4xl text-yellow-500 mb-6'>Welcome to the Bible API</h1>
+        <p className='text-2xl text-red-600 mb-12'>Select a page to continue</p>
+        <div className="flex">
+          <Link href="/form" className="inline-block py-3 px-6 bg-yellow-500 hover:bg-red-500 text-white rounded mr-4 transition-colors">
+            Form View
+          </Link>
+          <Link href="/view-data" className="inline-block py-3 px-6 bg-yellow-500 hover:bg-red-500 text-white rounded transition-colors">
+            Data View
+          </Link>
+        </div>
     </main>
     </RootLayout>
   )
