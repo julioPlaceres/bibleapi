dee62bd78c8192d83ec3144f1a0b7ffb6a2bf993
dreamheart <leondreamheart@outlook.com>
2023-10-15 20:27:41 -0400

    Updated main btns page and navbar

--- client/app/components/navigation/Navbar.tsx
+++ client/app/components/navigation/Navbar.tsx
@@ -1,43 +1,74 @@
-'use client';
-import React from 'react';
-import { useState } from 'react';
-import Link from 'next/link'
+"use client";
+import React from "react";
+import { useState } from "react";
+import Link from "next/link";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   function toggleMenu() {
-    console.log('toggleMenu');
+    console.log("toggleMenu");
     setIsOpen(!isOpen);
   }
 
   return (
-    <header className='w-full absolute z-10'>
+    <header className="w-full absolute z-10">
       <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <Link href="/" className="font-semibold text-xl tracking-tight">BibleApi</Link>
+          <Link href="/" className="font-semibold text-xl tracking-tight">
+            BibleApi
+          </Link>
         </div>
         <div className="block sm:hidden">
-          <div onClick={toggleMenu} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v15z" /></svg>
+          <div
+            onClick={toggleMenu}
+            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          >
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v15z" />
+            </svg>
           </div>
         </div>
-        <div className={`${isOpen ? 'block' : 'hidden'} w-full block flex-grow sm:flex sm:items-center sm:w-auto`}>
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } w-full block flex-grow sm:flex sm:items-center sm:w-auto`}
+        >
           <div className="text-sm sm:flex-grow">
-            <Link href="/form" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
-              Form
+            <Link
+              href="/form"
+              className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              View Form
             </Link>
-            <Link href="/view-data" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
+            <Link
+              href="/view-data"
+              className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"
+            >
               View Data
             </Link>
-            <Link href="/contact" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white">
-              Contact Us
+            <Link
+              href="/search"
+              className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Search Data
+            </Link>
+            <Link
+              href="/contact"
+              className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white"
+            >
+              Contact Me
             </Link>
           </div>
         </div>
       </nav>
     </header>
-  )
+  );
 };
 
 export default NavBar;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,18 +1,35 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-24 bg-cover bg-center" style={{backgroundImage: 'url(/cross.jpg)' }}>
-      <h1 className='text-4xl text-yellow-500 mb-6'>Welcome to the Bible API</h1>
-        <p className='text-2xl text-red-600 mb-12'>Select a page to continue</p>
-        <div className="flex">
-          <Link href="/form" className="inline-block py-3 px-6 bg-yellow-500 hover:bg-red-500 text-white rounded mr-4 transition-colors">
-            Form View
-          </Link>
-          <Link href="/search" className="inline-block py-3 px-6 bg-yellow-500 hover:bg-red-500 text-white rounded transition-colors">
-            Data View
-          </Link>
-        </div>
+    <main
+      className="min-h-screen flex flex-col items-center justify-center p-24 bg-cover bg-center"
+      style={{ backgroundImage: "url(/cross.jpg)" }}
+    >
+      <h1 className="text-4xl text-yellow-500 mb-6">
+        Welcome to the Bible API
+      </h1>
+      <p className="text-2xl text-red-600 mb-12">Select a page to continue</p>
+      <div className="flex">
+        <Link
+          href="/form"
+          className="inline-block py-3 px-6 bg-yellow-500 hover:bg-red-500 text-white rounded mr-4 transition-colors"
+        >
+          View Form
+        </Link>
+        <Link
+          href="/view-data"
+          className="inline-block py-3 px-6 bg-yellow-500 hover:bg-red-500 text-white rounded mr-4 transition-colors"
+        >
+          View Data
+        </Link>
+        <Link
+          href="/search"
+          className="inline-block py-3 px-6 bg-yellow-500 hover:bg-red-500 text-white rounded transition-colors"
+        >
+          Search Data
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
